3cd01171ff5d77db95d0f0345c1c346a358848a5
Casing Chu <casingchu@nationnotice-lm.corp.yahoo.com>
2015-01-20 19:25:07 -0800

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Casing Chu on 1/20/15.
+//  Copyright (c) 2015 casing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Casing Chu on 1/20/15.
+//  Copyright (c) 2015 casing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
